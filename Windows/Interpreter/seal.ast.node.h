98004ae480a7c4d2f4d60f83d0e8a808a6bd9854
seal-language <git@seal-lang.org>
2021-02-17 19:43:20 +0800

    Start building the AST syntax tree (Perfect AST node)

--- Windows/Interpreter/seal.ast.node.h
+++ Windows/Interpreter/seal.ast.node.h
@@ -8,3 +8,68 @@
 //       @birth       :  2021/2.17
 //       @operator    :  Margoo
 //
+
+#include <string>
+#include <vector>
+
+////////////////////////////////////////////////////////////////////////////////////////////
+//   _ast_node_assignment_expression[typedef with seal_ast_exp_info] :
+//          @description :   This class stores information about assignment expression of AST nodes
+//          @birth       :   2021/2.17
+//          @operator    :   Margoo
+typedef struct _ast_node_assignment_expression {
+	////////////////////////////////////////////////////////////////////////////////////////////
+	//   _ast_node_callee_expression[typedef with seal_callee_exp_info] (sub class) :
+	//          @description :   This class stores information about function call
+	//          @birth       :   2021/2.17
+	//          @operator    :   Margoo
+	typedef struct _ast_node_callee_expression {
+		// Callee identifier (i.e. function name)
+		std::string called_identifier = "";
+
+		// Callee arguments expression
+		std::vector<_ast_node_assignment_expression> calle_arguments;
+	} sealcallee_exp_info;
+
+	// Inverse Polish expression lvalue
+	_ast_node_assignment_expression* left_value_tree  = nullptr;
+	// Inverse Polish expression rvalue
+	_ast_node_assignment_expression* right_value_tree = nullptr;
+
+	// Median (i.e. operator)
+	std::string operator_symbol = "";
+
+	// Indicates the language type
+	std::string expression_type = "";
+
+	// If there is no need to store in a tree structure, then the corresponding value is
+	std::string value = "";
+} seal_ast_exp_info;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//   _ast_node_variable_declaration_info[typedef with seal_ast_var_info] :
+//          @description :   This class stores information about variable declarations of AST nodes
+//          @birth       :   2021/2.17
+//          @operator    :   Margoo
+typedef struct _ast_node_variable_declaration_info {
+	// Equivalent to variable name
+	std::string variable_declarator = "";
+
+	// Variable assignment expression (reverse Polish median)
+	_ast_node_assignment_expression init_expression;
+} seal_ast_var_info;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//   _ast_node_function_declaration_info[typedef with seal_ast_fnc_info] :
+//          @description :   This class stores information about function declarations of AST nodes
+//          @birth       :   2021/2.17
+//          @operator    :   Margoo
+typedef struct _ast_node_function_declaration_info {
+	// Equivalent to function name
+	std::string function_declarator = "";
+
+	// Function parameters (with vector)
+	std::vector<_ast_node_variable_declaration_info> parameter;
+} seal_ast_fnc_info;
+
+
