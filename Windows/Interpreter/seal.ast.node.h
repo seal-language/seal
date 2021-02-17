6b6fec00c450ff8bbc0b3970ea3f59ef0ce09174
seal-language <git@seal-lang.org>
2021-02-17 18:42:09 +0800

    Add AST syntax tree

---
+++ Windows/Interpreter/seal.ast.node.h
@@ -0,0 +1,10 @@
+#pragma once
+
+////////////////////////////////////////////////////////////////////////////////////////////
+//         S E A L  P R O G R A M M I N G  L A N G U A G E (seal-lang.org)
+//  
+//  seal.ast.node.h : 
+//       @description :  This header file defines the node class of the seal syntax tree
+//       @birth       :  2021/2.17
+//       @operator    :  Margoo
+//
